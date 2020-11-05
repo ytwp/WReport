0f3400ddf83132f4be85c995f16134ecefd2073e
weipeng <ytwppp@163.com>
2020-11-05 22:58:38 +0800

    1

---
+++ wreport-admin/src/main/java/wang/yeting/wreport/admin/WReportAdminApplication.java
@@ -0,0 +1,16 @@
+package wang.yeting.wreport.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author weipeng
+ */
+@SpringBootApplication
+public class WReportAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WReportAdminApplication.class, args);
+	}
+
+}
---
+++ wreport-admin/src/test/java/wang/yeting/wreport/admin/WReportAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package wang.yeting.wreport.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WReportAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
